bcb0c4954e1b8d3135993605453151788f8b203b
sa2k <sa2k@sa2ks-MacBook-Pro.local>
2025-10-29 18:23:31 +0700

    update form

--- lib/src/layout/unicos_form.dart
+++ lib/src/layout/unicos_form.dart
@@ -7,12 +7,14 @@ class UnicosForm extends StatelessWidget {
   final List<Widget> children;
   final Widget? footer;
   final Widget? header;
+  final List<int>? expandedColumns;
 
   const UnicosForm({
     super.key,
     required this.children,
     this.footer,
     this.header,
+    this.expandedColumns,
   });
 
   @override
@@ -20,6 +22,25 @@ class UnicosForm extends StatelessWidget {
     return LayoutBuilder(
       builder: (ctx, constraints) {
         int column = (constraints.maxWidth / (400 + 17)).ceil();
+
+        List<dynamic> rows = List.empty(growable: true);
+
+        children.forEachIndexed((index, child) {
+          if (expandedColumns?.contains(index) == true) {
+            rows.add(child);
+
+            if (index == 0) {
+              
+            }
+          } else {
+            if (index % column == 0) {
+              rows.add(List.from([child]));
+            } else {
+              rows.last.add(child);
+            }
+          }
+        });
+
         final List<List<Widget>> tables = children.isEmpty
             ? []
             : children.slices(column).toList();
