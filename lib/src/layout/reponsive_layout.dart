0b88a3d3191cdd6d2b570a243b041921aa15a2bf
sa2k <sa2k@sa2ks-MacBook-Pro.local>
2025-12-09 15:18:48 +0700

    update scale screen

--- lib/src/layout/reponsive_layout.dart
+++ lib/src/layout/reponsive_layout.dart
@@ -17,24 +17,16 @@ class ReponsiveLayout extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final double scale = size.width >= 600 ? 1 : size.width / 600;
     final double height = size.height / scale;
-
-    return Stack(
-      children: [
-        Positioned(
-          top: 0,
-          left: 0,
-          child: Transform.scale(
-            alignment: Alignment.topLeft,
-            scale: scale,
-            child: Container(
-              color: backgroundColor,
-              width: fixedWidth ?? (scale == 1 ? size.width : 600),
-              height: height,
-              child: child,
-            ),
-          ),
-        ),
-      ],
+    final double width = scale == 1 ? size.width : 600;
+    return Transform.scale(
+      alignment: Alignment.topLeft,
+      scale: scale,
+      child: Container(
+        color: backgroundColor,
+        width: width,
+        height: height,
+        child: child,
+      ),
     );
   }
 }
